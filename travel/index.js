486ace737411fadf2f0e26bc0bcabcea0a077745
AntonFartovii <72494592+AntonFartovii@users.noreply.github.com>
2022-06-29 01:43:11 +0300

    Update index.js

--- travel/index.js
+++ travel/index.js
@@ -5,41 +5,41 @@ console.log (`
 4. Требования к css + 12\n
 5. Интерактивность, реализуемая через css +20\n`)
 
-let state;
-const popup = {
-    state,
-    show: () => {
-        if (this.state !== true) {
-            popupAuth.style.display = 'block';
-            wrapper.style.opacity = '0.7';
-            this.state = true;
-        } else {
-            popupAuth.style.display = 'none';
-            wrapper.style.opacity = '1';
-            this.state = false;
-        }
-    },
-    disable: () => {
-        if (this.state === true) {
-            popupAuth.style.display = 'none';
-            wrapper.style.opacity = '1';
-            this.state = false;
-        }
-    }
-}
 
+
+let state
+let popup
 const buttonAuth =  document.querySelector('.btn-login')
 const popupAuth  = document.querySelector('.popup-auth')
-const linkReg = document.querySelector('.link-reg')
+const popupReg  = document.querySelector('.popup-reg')
 const wrapper = document.querySelector('.wrapper')
+const linkReg = document.querySelector('.link-reg')
+const linkIn = document.querySelector('.link-in')
 
-buttonAuth.addEventListener('click', () => {
-    popup.show()
+buttonAuth.addEventListener ( 'click', (event) => {
+    console.log (event)
+    state !== true ? show( popupAuth ) : hide( popup )
+} )
+
+linkReg.addEventListener('click', () => {
+    hide( popupAuth )
+    show( popupReg )
 })
 
-// wrapper.addEventListener('click', () => {
-//     popup.disable()
-// })
+linkIn.addEventListener('click', () => {
+    hide( popupReg )
+    show( popupAuth )
 
+})
 
-
+function show( obj ) {
+        obj.style.display = 'block';
+        wrapper.classList.add('active');
+        popup = obj
+        state = true;
+    }
+function hide( obj ) {
+        obj.style.display = 'none';
+        wrapper.classList.remove ( 'active' )
+        state = false;
+    }
